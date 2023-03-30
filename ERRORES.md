974804b75cc6291c51aebc8d981745979f800d68
Minerva Gamiz <minerva.gamiz.mejias@cieep.com>
2023-03-30 10:15:20 +0200

    updated: function getSize() at Directory class optimized

---
+++ ERRORES.md
@@ -0,0 +1,5 @@
+## ERRORES
+
+### Principio abierto / cerrado
+Se han modificado los if/else anidados en la clase FileManager de modo que ya no existen pero mantienen su función. 
+
--- src/com/kreitek/files/Directory.java
+++ src/com/kreitek/files/Directory.java
@@ -1,5 +1,7 @@
 package com.kreitek.files;
 
+import com.kreitek.service.FileManager;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -39,11 +41,7 @@ public class Directory extends FileSystemItemBase implements FileSystemItem {
 
     @Override
     public int getSize() {
-        int size = 0;
-        for (FileSystemItem file: files) {
-            size += file.getSize();
-        }
-        return size;
+        return FileManager.calculateSize(files);
     }
 
     @Override
